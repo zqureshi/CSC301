9ce6be1581f4dc6c47d016e8ba3f7eb1b7828b98
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-10-25 09:20:47 +0000

    modified view list_users to include a thead and tbody for better presentation
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@13 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/list_users.php
+++ trunk/system/application/views/list_users.php
@@ -13,20 +13,24 @@
 
     <div class="span-24 last">
       <table>
-        <tr>
-          <th>ID</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>E-Mail</th>
-        </tr>
-        <?php foreach($query as $row): ?>
-        <tr>
-          <td><?= $row->id ?></td>
-          <td><?= $row->first_name ?></td>
-          <td><?= $row->last_name ?></td>
-          <td><?= $row->email ?></td>
-        </tr>
-        <?php endforeach; ?>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>E-Mail</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($query as $row): ?>
+          <tr>
+            <td><?= $row->id ?></td>
+            <td><?= $row->first_name ?></td>
+            <td><?= $row->last_name ?></td>
+            <td><?= $row->email ?></td>
+          </tr>
+          <?php endforeach; ?>
+        </tbody>
       </table>
   </div>
   </body>
