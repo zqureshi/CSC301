fd2a8fc30b91fded617a4444e7f9f4e18eee7e85
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-10-27 07:31:39 +0000

    added model to implement recovery hashes
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@22 2fcfc99d-841f-4024-9191-67b2ebf9adcd

---
+++ trunk/system/application/models/recovery_hash.php
@@ -0,0 +1,28 @@
+<?php
+
+class Recovery_hash extends Model {
+  var $id   = '';
+  var $user = '';
+
+  function Recovery_hash()
+  {
+    parent::Model();
+  }
+
+  function add_hash($user_id)
+  {
+    $this->load->model('Booking_user');
+    $result = $this->Booking_user->get_user('id', $user_id);
+
+    $user = $result[0];
+    $this->id   = dohash($user->id . $user->email . $user->password);
+    $this->user = $user_id;
+
+    $this->db->insert('Recovery', $this);
+  }
+
+  function remove_hash($hash){
+    $this->db->where('id', $hash);
+    $this->db->delete('Recovery');
+  }
+}
