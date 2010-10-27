4897adb147f8b1454078ac9ad2a81c7c0b68e675
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-10-27 23:11:02 +0000

    added password reset form
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@30 2fcfc99d-841f-4024-9191-67b2ebf9adcd

---
+++ trunk/system/application/views/reset_pass_form.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <title>Reset Password</title>
+    <?php $this->load->view('includes') ?>
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="span-24 prepend-top last">
+        <h1>Reset Password</h1>
+        <hr/>
+      </div>
+
+      <div class="span-16 prepend-4 append-4 last">
+        <?= form_open('recovery/reset_password') ?>
+        <table>
+            <td>New Password</td>
+            <td><?= form_password('password') ?></td>
+          </tr>
+          <tr>
+            <td>Confirm Password</td>
+            <td><?= form_password('password2') ?></td>
+          </tr>
+        </table>
+        <?= form_submit('Reset', 'Reset') ?>
+        <?= form_close() ?>
+      </div>
+    </div>
+  </body>
+</html>
