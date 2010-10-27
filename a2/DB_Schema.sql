c1c9a1e5772797b6ab4caae3514a810c0c194332
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-10-27 22:27:26 +0000

    Added table to DB for storing recovery hashes
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@29 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- a2/DB_Schema.sql
+++ a2/DB_Schema.sql
@@ -1,17 +1,29 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.10
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 25, 2010 at 09:45 AM
--- Server version: 5.0.90
--- PHP Version: 5.2.12
+-- Generation Time: Oct 28, 2010 at 12:26 AM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Database: `aafjj9f2_booking_tool`
+-- Database: `labbooking`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Recovery`
+--
+
+CREATE TABLE IF NOT EXISTS `Recovery` (
+  `id` varchar(40) NOT NULL,
+  `user` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`user`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -19,12 +31,12 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `Users`
 --
 
-DROP TABLE IF EXISTS `Users`;
-CREATE TABLE `Users` (
-  `id` int(11) NOT NULL auto_increment,
-  `first_name` varchar(50) collate utf8_unicode_ci NOT NULL,
-  `last_name` varchar(50) collate utf8_unicode_ci NOT NULL,
-  `email` varchar(50) collate utf8_unicode_ci NOT NULL,
-  `password` text collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+CREATE TABLE IF NOT EXISTS `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
