7962920722b0cc92cb4131be472d06a7c163f315
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-10-28 05:36:11 +0000

    rename welcome_message.php to welcome_message_old.php and rename welcome_message_inprogress.php to welcome_message.php
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@41 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/welcome_message.php
+++ trunk/system/application/views/welcome_message.php
@@ -1,14 +1,16 @@
 <html>
 <head>
 <title>Online Lab Booing Tool</title>
+<link rel="stylesheet" href="styles.css" type="text/css" /> 
 <?php $this->load->view('includes') ?>
 </head>
 <body>
-<div class="container">
-  <div class="span-24 prepend-top last">
-    <h1>Lab Booking Tool</h1>
-    <hr/>
-  </div>
+<h1 align="center">
+
+
+<div class="center">
+<div class="container1"><span class="span-24 prepend-top last"><img src="frame_hor.png" width=100% height="30"></span></div>
+<div class="container"><div class="span-24 prepend-top last"><img src="logo_cropped.png" width="298" height="114"></div>
 
   <div class="span-8 prepend-8 append-8 last">
     <?= form_open('welcome/login') ?>
@@ -18,15 +20,17 @@
     <?= form_label('Password:', 'label_password') ?>
     <?= form_input('password', '') ?>
     <br/>
-    <hr class="space"/>
     <div class="prepend-2">
       <?= form_submit('Login', 'Login') ?>
     </div>
     <?= form_close() ?>
   </div>
+  
+ <div class="container1"><span class="span-24 prepend-top last"><img src="frame_hor.png" width=100% height="30"></span></div>
 
-  <div class="span-24 last" id="footer">
-  </div>
+  <div class="span-24 last" id="footer">  </div>
 </div>
+</div>
+</h1>
 </body>
 </html>
