d2b341609e3b7ac282581d168a02e1dbb087d712
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-10-28 01:37:11 +0000

    edit booking_user.php
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@35 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/models/booking_user.php
+++ trunk/system/application/models/booking_user.php
@@ -5,6 +5,7 @@ class Booking_user extends Model {
   var $first_name = '';
   var $last_name  = '';
   var $email      = '';
+  var $username   = '';
   var $password   = '';
 
   function Booking_user()
@@ -38,6 +39,7 @@ class Booking_user extends Model {
     $this->first_name = xss_clean($this->input->post('first_name'));
     $this->last_name  = xss_clean($this->input->post('last_name'));
     $this->email      = xss_clean($this->input->post('email'));
+    $this->username   = xss_clean($this->input->post('username'));
     $this->password   = dohash($this->input->post('password'));
 
     $this->db->insert('Users', $this);
