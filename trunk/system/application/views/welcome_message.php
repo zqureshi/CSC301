1abdfeabb3049f856cb5279c2fa33b211c4bdfe5
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-14 07:44:47 +0000

    center object
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@70 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/welcome_message.php
+++ trunk/system/application/views/welcome_message.php
@@ -15,20 +15,28 @@ body {
 <body>
 <div class="centered">
 <div><span class="span-24 prepend-top last"><img src="<?= site_url('img/frame_hor.png') ?>" width=100% height="30"></span></div>
-<div class="container"><div class="span-8 prepend-6 last"><img src="<?= site_url('img/logo_cropped.png') ?>" width="298" height="114"></div>
+<div class="container"><div class="span-8 prepend-6 last">
+  <div align="center"><img src="<?= site_url('img/logo_cropped.png') ?>" width="298" height="114"></div>
+</div>
 
   <div class="span-8 prepend-8 append-8 last">
-    <?= form_open('welcome/login') ?>
-    <?= form_label('Username:', 'label_username') ?>
-    <?= form_input('username', '') ?>
-    <br/>
-    <?= form_label('Password:', 'label_password') ?>
-    <?= form_password('password', '') ?>
-    <br/>
-    <div class="prepend-2">
-      <?= form_submit('Login', 'Login') ?>
+    <div align="center">
+      <?= form_open('welcome/login') ?>
+      <?= form_label('Username:', 'label_username') ?>
+      <?= form_input('username', '') ?>
+      <br/>
+      <?= form_label('Password:', 'label_password') ?>
+      <?= form_password('password', '') ?>
+      <br/>
     </div>
-    <?= form_close() ?>
+    <div class="prepend-2">
+      <div align="center">
+        <?= form_submit('Login', 'Login') ?>
+        </div>
+    </div>
+    <div align="center">
+      <?= form_close() ?>
+      </div>
   </div>
   
  <div><span class="span-24 prepend-top last"><img src="<?= site_url('img/frame_hor.png') ?>" width=100% height="30"></span></div>
