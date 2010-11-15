28e24fa826dc152e595f30bcfbf1a02dfbf89ff7
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-15 04:31:13 +0000

    Fixed Error with logo not being centred.  Finished Ticket 9
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@83 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/welcome_message.php
+++ trunk/system/application/views/welcome_message.php
@@ -1,27 +1,28 @@
 <html>
 <head>
 <title>Online Lab Booing Tool</title>
-<link rel="stylesheet" href="<?= site_url('css/styles.css') ?>" type="text/css" /> 
+<link rel="stylesheet" href="<?= site_url('css/styles.css') ?>" type="text/css" />
 <?php $this->load->view('includes') ?>
 <link href="../../../css/styles.css" rel="stylesheet" type="text/css">
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><style type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
 <!--
 body {
 	background-color: #FFFFFF;
 	margin-bottom: 0px;
 }
 -->
-</style></head>
+</style>
+</head>
 <body>
 <div class="centered">
-<div><span class="span-24 prepend-top last"><img src="<?= site_url('img/frame_hor.png') ?>" width=100% height="30"></span></div>
-<div class="container"><div class="span-8 prepend-6 last">
-    <p class="centeredImage"><img src="<?= site_url('img/logo_cropped.png') ?>" width="298" height="114"></p>
-</div>
-
-  <div class="span-8 prepend-8 append-8 last">
-    
-      <div align="center">
+  <div><span class="span-24 prepend-top last"><img src="<?= site_url('img/frame_hor.png') ?>" width=100% height="30"></span></div>
+  <div class="container">
+    <div class="centeredImage">
+      <img src="<?= site_url('img/logo_cropped.png') ?>" width="298" height="114">
+    </div>
+    <div class="span-8 prepend-8 append-8 last">
+      <div align="center"> <span class="centeredInside">
         <?= form_open('welcome/login') ?>
         <?= form_label('Username:', 'label_username') ?>
         <?= form_input('username', '') ?>
@@ -29,25 +30,17 @@ body {
         <?= form_label('Password:', 'label_password') ?>
         <?= form_password('password', '') ?>
         <br/>
-        </div>
-      <div class="prepend-2">
-      
-        <div align="center">
-          <?= form_submit('Login', 'Login') ?>
-        </div>
-      </div>
-    
+        </span></div>
       <div align="center">
-        <?= form_close() ?>
-      </div>
+        <?= form_submit('Login', 'Login') ?>
+        </div>
+      <?= form_close() ?>
+    </div>
+    <div>
+      <div align="center"><span class="span-24 prepend-top last"><img src="<?= site_url('img/frame_hor.png') ?>" width=100% height="30"></span></div>
+    </div>
+    <div class="span-24 last" id="footer"> </div>
   </div>
-  
- <div>
-   <div align="center"><span class="span-24 prepend-top last"><img src="<?= site_url('img/frame_hor.png') ?>" width=100% height="30"></span></div>
- </div>
-
-  <div class="span-24 last" id="footer">  </div>
-</div>
 </div>
 </body>
 </html>
