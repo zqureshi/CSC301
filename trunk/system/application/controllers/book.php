92931c2b5f0437ec5c7b794e7e4a3fa0111e7694
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-15 21:46:45 +0000

    Add Frames
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@86 2fcfc99d-841f-4024-9191-67b2ebf9adcd

---
+++ trunk/system/application/controllers/book.php
@@ -0,0 +1,12 @@
+<?php
+
+class Book extends Controller {
+
+	function Book()
+	{
+		parent::Controller();	
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./system/application/controllers/welcome.php */
---
+++ trunk/system/application/views/book.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<frameset rows="52,*" cols="*" frameborder="no" border="0" framespacing="0">
+  <frameset rows="64*,268*" cols="596">
+    <frame src="top_frame.php" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="top_frame" />
+  
+  <frameset rows="*" cols="301,*" framespacing="0" frameborder="no" border="0">
+    <frame src="side_frame.php" name="leftFrame" scrolling="No" noresize="noresize" id="leftFrame" title="side_frame" />
+    <frame src="bookroom.php" name="mainFrame" id="mainFrame" title="My Calendar" />
+  </frameset>
+</frameset>
+<noframes><body>
+</body>
+</noframes></html>
