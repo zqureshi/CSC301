d6be9e2c37f12f2cbdca30ad06d6a7bbb5d9bddf
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-15 22:00:21 +0000

    Edit book.php
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@88 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/book.php
+++ trunk/system/application/views/book.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>My Calendar</title>
+<link rel="stylesheet" href="<?= site_url('css/styles.css') ?>" type="text/css" />
+<?php $this->load->view('includes') ?>
+<link href="../../../css/styles.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+body {
+	background-color: #FFFFFF;
+	margin-bottom: 0px;
+	text-align: center
+}
+-->
+</style>
 </head>
 
 <frameset rows="52,*" cols="*" frameborder="no" border="0" framespacing="0">
