ee8a729b43859ba1d768c5ec5289a7fdb4e93041
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-16 01:24:58 +0000

    Edit frame layout
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@94 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/book.php
+++ trunk/system/application/views/book.php
@@ -4,10 +4,10 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Online Lab Booking Tool</title>
 </head>
-<frameset rows="40,*" cols="*" frameborder="no" border="0" framespacing="0">
-  <frame src="top" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="top_frame" />
-  <frameset rows="*" cols="298,*" framespacing="0" frameborder="no" border="0">
+<frameset rows="*" cols="300, *" frameborder="no" border="0" framespacing="0">
     <frame src="side" name="leftFrame" scrolling="No" noresize="noresize" id="leftFrame" title="side_frame" />
+  <frameset rows="40, *" cols="*" framespacing="0" frameborder="no" border="0">
+    <frame src="top" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="top_frame" />
     <frame src="bookroom" name="mainFrame" id="mainFrame" title="My Calendar" />
   </frameset>
 </frameset>
