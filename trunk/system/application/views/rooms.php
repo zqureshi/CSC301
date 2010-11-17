ba9d920ba88c8c3378ab99c3baf920d5957c8ab3
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-17 17:49:13 +0000

    add missing file and change paths to layout.css in the view file
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@98 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/rooms.php
+++ trunk/system/application/views/rooms.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"	xml:lang="en">
 <head>
-<link rel="stylesheet" type="text/css" href="/codeig/system/application/layout.css" />
+<link rel="stylesheet" type="text/css" href="../../../css/styles.css" />
 <title>My Calendar</title>
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="/codeig/system/application/style.css" />
+<link rel="stylesheet" type="text/css" href="../../../css/styles.css" />
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
 </head>
 <body>
