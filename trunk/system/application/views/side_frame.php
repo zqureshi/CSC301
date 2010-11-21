572411c27cf5d7565fd1e8206c2cdd8b28c651cf
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-21 20:39:28 +0000

    Change Your current Booking layout
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@136 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/side_frame.php
+++ trunk/system/application/views/side_frame.php
@@ -11,23 +11,23 @@
     <td height="117" colspan="2"><a href="bookroom" target="mainFrame"><img src="<?= site_url('img/logo_cropped.png')?>" width="288" height="108" border="0" /></a></td>
   </tr>
   <tr>
-    <td colspan="2"><p class="center">Your Current Bookings</p></td>
+    <td colspan="2"><p class="center">Your Current Bookings</p>      </td>
   </tr>
   <tr>
     <td width="20"></td>
     <td width="265"><p class="center">
-        <?php foreach($query as $row): ?>
-        <?= $link = "<br \n>";
+      <?php foreach($query as $row): ?>
+      <?= $link = "<br \n>";
                 $link = "/thirdPage/index/";
                 $link .= date_format(date_create($row->Date), "Y/m/d") ;
                 $link .= "/$row->rID/$row->tID";
-                $temp1 = date_format(date_create($row->Date), "F j, Y") ;
+                $temp1 = date_format(date_create($row->Date), "M j, Y") ;
 		        $temp2 = $this->rooms_model->get_slot_name($row->tID) ; ?>
-      <ul>
+      
         <li>
-          <?= anchor($link, $temp1 .' between '. $temp2,'target="mainFrame"'); ?>
+          <?= anchor($link, $temp1 .' : '. $temp2,'target="mainFrame"'); ?>
         </li>
-      </ul>
+      
       <?php endforeach;?>
       </p>
     </td>
@@ -35,3 +35,4 @@
 </table>
 </body>
 </html>
+
