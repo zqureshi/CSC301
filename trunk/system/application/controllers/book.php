53b95b776606b63342d497296416bab869ef9df4
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-23 16:44:01 +0000

    Removed side and top from controllers and put them inside book
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@137 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/controllers/book.php
+++ trunk/system/application/controllers/book.php
@@ -4,24 +4,38 @@ class Book extends Controller {
 
 	function Book()
 	{
-		parent::Controller();	
-	}
-
-	function index()
-	{
+		parent::Controller();
 		/* Check if session is valid first */
 		if($this->session->userdata('id') == FALSE)
 		{
 			redirect('/welcome');
 		}
-		$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
+		$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");	
+	}
 
+	function side()
+	{
+		$this->load->model('side_model');
+
+		$data['query'] = $this->side_model->current_booking();
+		$this->load->view('side_frame', $data);
+	}
+
+	function top()
+	{
+		$this->load->model('booking_user');
+                $uid = $this->session->userdata('id');
+                $data['id'] = $this->booking_user->get_username($uid);
+		$this->load->view('top_frame', $data);
+	}
+
+	function index()
+	{
 		$this->load->view('book');
-		$this->load->view('side_frame');
-		$this->load->view('top_frame');
 		$this->load->view('bookroom');
 	}
 }
 
 /* End of file book.php */
 /* Location: ./system/application/controllers/book.php */
+
--- trunk/system/application/views/book.php
+++ trunk/system/application/views/book.php
@@ -5,9 +5,9 @@
 <title>Online Lab Booking Tool</title>
 </head>
 <frameset rows="*" cols="300, *" frameborder="no" border="0" framespacing="0">
-    <frame src="side" name="leftFrame" scrolling="No" noresize="noresize" id="leftFrame" title="side_frame" />
+    <frame src="book/side" name="leftFrame" scrolling="No" noresize="noresize" id="leftFrame" title="side_frame" />
   <frameset rows="40, *" cols="*" framespacing="0" frameborder="no" border="0">
-    <frame src="top" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="top_frame" />
+    <frame src="book/top" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="top_frame" />
     <frame src="bookroom" name="mainFrame" id="mainFrame" title="My Calendar" />
   </frameset>
 </frameset>
