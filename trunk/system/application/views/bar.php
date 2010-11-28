b90352dc252479d03ec5371e81dadee473ce4beb
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-28 19:29:20 +0000

    update bar.php to get rid of tables
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@187 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/bar.php
+++ trunk/system/application/views/bar.php
@@ -21,31 +21,21 @@ if($is_admin == TRUE)
 <HR>
 <!-- Side Panel -->
 <div class="side">
-<table width="295" border="0">
-  <tr>
-    <td height="117" colspan="2"><a href="/bookroom"><img src="<?= site_url('img/logo_cropped.png')?>" width="288" height="108" border="0" /></a></td>
-  </tr>
-  <tr>
-    <td colspan="2"><p class="center">Your Current Bookings</p></td>
-  </tr>
-  <tr>
-    <td width="20"></td>
-    <td width="265"><p class="center">
-        <?php foreach($query as $row): ?>
-        <?= $link = "<br \n>";
-                $link = "/thirdPage/index/";
-                $link .= date_format(date_create($row->Date), "Y/m/d") ;
-                $link .= "/$row->rID/$row->tID";
-                $temp1 = date_format(date_create($row->Date), "M d, Y") ;
-		        $temp2 = $this->rooms_model->get_slot_name($row->tID) ; ?>
+    <a href="/bookroom"><img src="<?= site_url('img/logo_cropped.png')?>" width="288" height="108" border="0" /></a>
+    <p class="center">Your Current Bookings</p></td>
+    <p class="center">
+      <?php foreach($query as $row): ?>
+      <?= $link = "<br \n>";
+          $link = "/thirdPage/index/";
+          $link .= date_format(date_create($row->Date), "Y/m/d") ;
+          $link .= "/$row->rID/$row->tID";
+          $temp1 = date_format(date_create($row->Date), "M d, Y") ;
+	  $temp2 = $this->rooms_model->get_slot_name($row->tID) ; ?>
       <li>
         <?= anchor($link, $temp1 .' : '. $temp2); ?>
       </li>
       <?php endforeach;?>
       </p>
-    </td>
-  </tr>
-</table>
 </div>
 </body>
 </html>
