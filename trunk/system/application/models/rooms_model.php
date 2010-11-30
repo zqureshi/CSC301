5de0998a0a16e8616500804babef989916b1a8d5
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-30 23:27:10 +0000

    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@217 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/models/rooms_model.php
+++ trunk/system/application/models/rooms_model.php
@@ -249,6 +249,9 @@ class Rooms_model extends Model {
 		$numOfSlots = $this->get_number_of_slots();
 		$numOfRooms = $this->get_number_of_rooms();
 		
+		$this->data .= "<center>$year-$month-$day</center>"."\n";
+		$this->data .= "<center><a href='/bookroom/index/$year/$month/'>Back to Calendar</a></center>"."\n";
+
 		$this->data .= "<table border='0' cellpadding='0' cellspacing='0' align=center>"."\n";
 		$this->data .= "	<tr>"."\n";
 		$this->data .= "		<td align=center><center>$year-$month-$day</center></td>"."\n";
