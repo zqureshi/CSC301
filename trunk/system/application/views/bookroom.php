f6a588364e9ccdbde4a2df45ff7235a651daf1b4
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-11-30 23:22:54 +0000

    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@215 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/views/bookroom.php
+++ trunk/system/application/views/bookroom.php
@@ -1,6 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"	xml:lang="en">
 <head>
 <link rel="stylesheet" href="<?= site_url('css/layout.css') ?>" type="text/css" />
 <title>My Calendar</title>
@@ -20,6 +19,13 @@
 			window.location.replace(thePage + day_num);
 		});	
 		$('.calendar .day_booked').click(function() {
+			day_num = $(this).find('.day_num').html();
+			<?php 
+				echo "var thePage = \"/rooms/index/$year/$month/\";";
+			?>
+			window.location.replace(thePage + day_num);
+		});	
+		$('.calendar .current_day').click(function() {
 			day_num = $(this).find('.day_num').html();
 			<?php 
 				echo "var thePage = \"/rooms/index/$year/$month/\";";
