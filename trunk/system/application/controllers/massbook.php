eed096ccef44aae68cc69521fefce3e1781deb29
g9mike <g9mike@2fcfc99d-841f-4024-9191-67b2ebf9adcd>
2010-12-01 01:41:22 +0000

    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2010-09/Quebec@239 2fcfc99d-841f-4024-9191-67b2ebf9adcd

--- trunk/system/application/controllers/massbook.php
+++ trunk/system/application/controllers/massbook.php
@@ -52,7 +52,6 @@ class Massbook extends Controller {
 			if($this->rooms_model->validate_date($start_date) && $this->rooms_model->validate_date($end_date)){
 				echo $this->rooms_model->validate_date($start_date) ;
 				echo $this->rooms_model->validate_date($end_date);
-				echo "AAAAAAAAAAAAAAAAAAAAAAAAA" ;
 				$this->rooms_model->massbook($start_date,$end_date,$course,$notes,$days,$roomname,$slotname,$override,$user);
 			}else{
 				// errNo 2 means there are errors in the date format.
@@ -65,8 +64,7 @@ class Massbook extends Controller {
 			$errNo = 1 ;
 			redirect("/massbook/index/$errNo","location");
 		}
-		echo "BBBBBBBBBBBBBBBBBB" ;
-		//redirect("/bookroom","location");
+		redirect("/bookroom","location");
 	}	
 
 	function test (){
